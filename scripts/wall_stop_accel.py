6b472d1ca3ed40ff25af080c249c169e4cacc31b
ishigem <ishigem@gmail.com>
2017-04-18 14:15:37 +0900

    wall stop accel 1

--- scripts/wall_stop_accel.py
+++ scripts/wall_stop_accel.py
@@ -9,17 +9,25 @@ class WallStop():
         self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
         self.sensor_values = LightSensorValues()
-        rospy.Subscriver('/lightsensors', LightSensorValues, self.callback)
+        rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
 
-    def callback(self.messages):
+    def callback(self,messages):
         self.sensor_values = messages
 
     def run(self):
         rate = rospy.Rate(10)
         data = Twist()
 
+        accel = 0.02
+        data.linear.x = 0.0
+        
         while not rospy.is_shutdown():
-            data.linear.x = 0.2 if self.sensor_values.sum_all < 500 else 0.0
+            data.linear.x += accel
+
+            if self.sensor_values.sum_all >= 50: data.linear.x = 0.0
+            elif data.linear.x <= 0.2:           data.linear.x = 0.2
+            elif data.linear.x >= 0.8:           data.linear.x = 0.8
+
             self.cmd_vel.publish(data)
             rate.sleep()
 
@@ -28,7 +36,7 @@ if __name__ == '__main__':
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
-    rospy.ServiceProxy('/motor_on',Trigger).call)
+    rospy.ServiceProxy('/motor_on',Trigger).call()
     WallStop().run()
 
 
