0845b0d53f24eaf156d48aa6ce6dc5d962b82a46
ishigem <ishigem@gmail.com>
2017-04-19 13:56:40 +0900

    wall trace add

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -15,7 +15,7 @@ class WallStopTest(unuttest.TestCase):
             left = int(lf.readline().rstrip())
             right = int(rf.readline().rstrip())
 
-       return left, right
+        return left, right
 
     def test_io(self):
         left, right = self.set_and_get(400,100,100,0) #total: 600
